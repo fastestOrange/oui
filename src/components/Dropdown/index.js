cf341589d5247f84d06ade2647b0d6ceb1f44d49
inaki.aranzadi@optimizely.com <inaki.aranzadi@optimizely.com>
2018-01-26 11:55:57 -0800

    Fix Dropdown errors and recompose conflict

--- src/components/Dropdown/index.js
+++ src/components/Dropdown/index.js
@@ -5,6 +5,7 @@ import { Manager, Popper, Target } from 'react-popper';
 import { withToggle } from '../../utils/recompose-utils';
 
 const Dropdown = withToggle(({
+  arrowIcon,
   buttonContent,
   children,
   hide,
@@ -31,98 +32,50 @@ const Dropdown = withToggle(({
     { ['oui-button--full soft--left text--left']: fullWidth }
   );
 
-  handleOnBlur() {
-    if (!this.state.overChildren) {
-      this.setState({
-        isOpen: false,
-      });
+  const iconClass = classNames(
+    'push-half--left',
+    {
+      ['oui-arrow-inline--down']: arrowIcon === true || arrowIcon === 'down',
+      ['oui-arrow-inline--left']: arrowIcon === 'left',
+      ['oui-arrow-inline--right']: arrowIcon === 'right',
+      ['oui-arrow-inline--up']: arrowIcon === 'up',
     }
-  }
+  );
 
-  handleToggle(event) {
-    this.setState({
-      isOpen: !this.state.isOpen,
-      overChildren: false,
-    });
-  }
-
-  render() {
-    const {
-      arrowIcon,
-      buttonContent,
-      children,
-      isDisabled = false,
-      fullWidth,
-      placement = 'bottom-start',
-      style,
-      testSection,
-      width = 200,
-      zIndex = 999,
-    } = this.props;
-
-    const groupClass = classNames(
-      'oui-dropdown-group',
-      { ['width--1-1']: fullWidth }
-    );
-
-    const buttonClass = classNames(
-      'oui-button',
-      { [`oui-button--${style}`]: style },
-      { ['oui-button--full soft--left text--left']: fullWidth }
-    );
-
-    const iconClass = classNames(
-      'push-half--left',
-      {
-        ['oui-arrow-inline--down']: arrowIcon === true || arrowIcon === 'down',
-        ['oui-arrow-inline--left']: arrowIcon === 'left',
-        ['oui-arrow-inline--right']: arrowIcon === 'right',
-        ['oui-arrow-inline--up']: arrowIcon === 'up',
-      }
-    );
-
-    return (
-      <Manager
-        data-ui-component={ true }
-        className={ groupClass }
-        data-test-section={ testSection }>
-        <Target>
-          <button
-            type='button'
-            className={ buttonClass }
-            disabled={ isDisabled }
-            onClick={ this.handleToggle }
-            onBlur={ this.handleOnBlur }>
-            <div className='flex'>
-              <div className='flex--1 truncate'>{ buttonContent }</div>
-              {
-                !!arrowIcon && arrowIcon !== 'none' && (
-                  <div className='text--right'><span className={ iconClass }></span></div>
-                )
-              }
-            </div>
-          </button>
-        </Target>
-        <Popper
-          placement={ placement }
-          className='oui-dropdown-children'
-          style={{
-            zIndex: zIndex,
-            position: 'absolute',
-            width: width,
-            marginTop: 2,
-            marginBottom: 2,
-            boxShadow: '0 2px 3px rgba(0,0,0,.1)',
-          }}
-          onMouseOver={ this.handleMouseOverChildren }
-          onMouseLeave={ this.handleMouseLeavingChildren }
-          onClick={ this.handleToggle } >
-          { this.state.isOpen && !isDisabled && children }
-        </Popper>
-      </Manager>
-    );
-  }
-}
+  return (
+    <Manager
+      data-ui-component={ true }
+      className={ groupClass }
+      data-test-section={ testSection }>
+      <Target>
+        <button
+          type='button'
+          className={ buttonClass }
+          disabled={ isDisabled }
+          onClick={ toggle }
+          onBlur={ hide }>
+          <div className='flex'>
+            <div className='flex--1 truncate'>{ buttonContent }</div>
+          </div>
+        </button>
+      </Target>
+      <Popper
+        placement={ placement }
+        className='oui-dropdown-children'
+        style={{
+          zIndex: zIndex,
+          position: 'absolute',
+          width: width,
+          marginTop: 2,
+          marginBottom: 2,
+          boxShadow: '0 2px 3px rgba(0,0,0,.1)',
+        }}
+        onClick={ hide } >
+        { isOpen && !isDisabled && children }
+      </Popper>
+    </Manager>
+  )
+});
 
 Dropdown.propTypes = {
   /** Arrow icon direction:
